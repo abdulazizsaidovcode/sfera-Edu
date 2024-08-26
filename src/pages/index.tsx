07e0cd47ee5cf2c62312b675400bffecebb04000
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-08-26 19:08:41 +0500

    subscribe and subscribed button finsihed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import ShinyButton from '@/components/magicui/shiny-button'
+import { AnimatedSubscribeButton } from '@/components/magicui/animated-subscribe-button'
+import { CheckIcon, ChevronRightIcon } from 'lucide-react'
 import React from 'react'
 
 function Index() {
     return (
         <div>
-            <ShinyButton
-                text='CANCEL'
-                className="bg-[#fff]"
+            <AnimatedSubscribeButton
+                buttonColor="#20CC6D"
+                buttonTextColor="#ffffff"
+                subscribeStatus={false}
+                initialText={
+                    <span className="group inline-flex items-center text-white">
+                        Subscribe{" "}
+                        <ChevronRightIcon className="ml-1 h-4 w-4 transition-transform duration-300 group-hover:translate-x-1 text-white  " />
+                    </span>
+                }
+                changeText={
+                    <span className="group inline-flex items-center">
+                        <CheckIcon className="mr-2 h-4 w-4 " />
+                        Subscribed{" "}
+                    </span>
+                }
             />
         </div>
     )
