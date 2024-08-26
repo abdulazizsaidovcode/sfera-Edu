70d9d2b43971c97635ffea8ec155f5e070fa3772
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-08-26 13:19:33 +0500

    pash

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './App.css'
+import BlurIn from './components/magicui/blur-in'
+import Marquee from './components/magicui/marquee'
+import ShinyButton from './components/magicui/shiny-button'
+import SparklesText from './components/magicui/sparkles-text'
+import TextRevealByWord from './components/magicui/text-reveal'
 
 function App() {
 
   return (
     <>
       hello
+      <BlurIn word={"Hello World"}></BlurIn>
+      <ShinyButton text='hello'></ShinyButton>
+      <TextRevealByWord text='hello my name is good boy'></TextRevealByWord>
+      <SparklesText text='salom jigarla' />
+      ddd <Marquee />
     </>
   )
 }
