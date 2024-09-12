eebd47fc934dd6821a77b925a8247f0b46022533
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-09-12 16:44:41 +0500

    npm run dev

--- src/components/custom/Sidebar/index.tsx
+++ src/components/custom/Sidebar/index.tsx
@@ -15,8 +15,8 @@ interface SidebarProps {
 
 const MenuItem = ({ title, to, pathname, icon }: { pathname: any, icon: any, title: string, to: string }) => {
   return (
-    <NavLink className={'text-black bg-[#6A9C89] '} to={to}>
-      <div className={`py-3 px-4 my-3 ${pathname === to ? 'bg-[#20CC6D] group relative shadow flex items-center gap-2.5  font-medium text-black duration-300 ease-in-out cursor-pointer border-none  dark:border-graydark  rounded-lg' : "bg-white hover:opacity-70 group relative shadow flex items-center gap-2.5 font-medium text-black duration-300 ease-in-out hover:bg-[#DAFBED]  cursor-pointer border-stroke  p-3 rounded-lg"}`}>
+    <NavLink className={'text-black bg-[#6A9C89]'} to={to}>
+      <div className={`py-3 px-4 my-3 ${pathname === to ? 'bg-[#063d36] group relative shadow-md flex items-center gap-2.5  font-medium text-white duration-300 ease-in-out cursor-pointer border-none rounded-lg' : "bg-white hover:opacity-70 group relative shadow flex items-center gap-2.5 font-medium text-black duration-300 ease-in-out hover:bg-[#DAFBED]  cursor-pointer border-stroke  p-3 rounded-lg"}`}>
         {icon}
         {title}
       </div>
@@ -59,12 +59,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen, isOpenModal, setIsOpenModal }: S
     if (setIsOpenModal && isOpenModal) setIsOpenModal(!isOpenModal);
   }
 
-
-
   return (
     <aside
       ref={sidebar}
-      className={`absolute left-0 top-0 z-999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-[#16423C] shadow-4 duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+      className={`absolute left-0 top-0 z-999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-[#16423C] shadow-4 duration-300 ease-linear  lg:static lg:translate-x-0 ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
@@ -95,86 +93,13 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen, isOpenModal, setIsOpenModal }: S
           </svg>
         </button>
       </div>
-      {/* <!-- SIDEBAR HEADER --> */}
 
-      <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
-        {/* <!-- Sidebar Menu --> */}
+      {/* <!-- Sidebar Menu --> */}
+      <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear flex-grow">
         <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
-
-          {/* <!-- Menu Group --> */}
-          <div className='flex flex-col '>
+          <div className='flex flex-col'>
             <ul className="mb-6 flex flex-col">
-              {/* <!-- Menu Item Dashboard ADMIN or SUPER_ADMIN --> */}
               <li>
-
-                {/* ADMIN MENUES */}
-
-                {/* <MenuItem
-                  title='Dashboard'
-                  icon={<LuLayoutDashboard size={20}/>}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Category'
-                  icon={<MdOutlineCategory  size={20}/>}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Group'
-                  icon={<MdGroup size={20}/>}
-                  pathname={location.pathname}
-                  to='/dd'
-                />
-                <MenuItem
-                  title='Teacher'
-                  icon={<FaChalkboardTeacher  size={20}/>}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Student'
-                  icon={<PiStudentFill size={20}/>}
-                  pathname={location.pathname}
-                  to='/'
-                /> */}
-
-                {/* TEACHER MENUES */}
-
-                {/* <MenuItem
-                  title='Dashboard'
-                  icon={<LuLayoutDashboard size={20} />}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Modul'
-                  icon={<VscFileSubmodule size={20} />}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Lesson'
-                  icon={<MdOutlinePlayLesson size={20} />}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Student'
-                  icon={<PiStudentFill size={20} />}
-                  pathname={location.pathname}
-                  to='/'
-                />
-                <MenuItem
-                  title='Homework'
-                  icon={<RiHomeOfficeLine size={20} />}
-                  pathname={location.pathname}
-                  to='/'
-                /> */}
-
-                {/* TEACHER MENUES */}
-
                 <MenuItem
                   title='Dashboard'
                   icon={<LuLayoutDashboard size={20} />}
@@ -186,15 +111,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen, isOpenModal, setIsOpenModal }: S
                   icon={<PiStudentFill size={20} />}
                   pathname={location.pathname}
                   to='/course'
-
                 />
               </li>
             </ul>
-            <ShinyButton onClick={toggleModal} icon={<IoIosLogOut size={25} />} text='Logout' className='bg-blue-400' />
           </div>
         </nav>
-        {/* <!-- Sidebar Menu --> */}
+      </div>
 
+      {/* <!-- Logout Button at the bottom --> */}
+      <div className="px-6 py-4 self-end w-full">
+        <ShinyButton onClick={toggleModal} icon={<IoIosLogOut size={25} />} text='Logout' className='bg-[#063d36] border-none w-full' />
       </div>
     </aside>
   );
