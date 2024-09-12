8a6c30ed0f952474b50f9cfb76a40b9fc7d93e09
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-09-12 18:38:33 +0500

    border lar taxt

--- src/pages/course/course.tsx
+++ src/pages/course/course.tsx
@@ -7,6 +7,8 @@ import { useCategory } from "@/context/logic/state-managment/course";
 import { useModule } from "@/context/logic/state-managment/module";
 import { config } from "@/context/api/token";
 import SlightFlip from "@/components/magicui/flip-text";
+import Particles from "@/components/magicui/particles";
+import ShineBorder from "@/components/magicui/shine-border";
 
 const Course = () => {
   const { setCategoryData, categoryData } = useCategory();
@@ -29,8 +31,8 @@ const Course = () => {
   }, []);
   console.log(moduleData);
   console.log(categoryData);
-  
-  
+
+
 
   const hasData = categoryData && categoryData.name;
   const accordionData = (moduleData || []).map((modulData: any) => ({
@@ -47,24 +49,30 @@ const Course = () => {
         className="text-3xl font-bold mb-6 text-gray-800" />
       {hasData ? (
         <>
-          <div className="bg-[#f0fff4] h-40 w-full flex items-center justify-center relative rounded-lg shadow-lg mb-6">
-            <div className="absolute inset-0 bg-black opacity-20 rounded-lg"></div>
+          <ShineBorder color={'#16423C'} borderWidth={1.5} duration={10} className="bg-[#fff] h-40 w-full flex items-center justify-center relative rounded-lg shadow-lg mb-6">
+            <Particles
+              className="absolute inset-0"
+              quantity={100}
+              ease={80}
+              color={'#16423C'}
+              refresh
+            />
             <div className="relative z-10 text-center">
-              <h2 className="text-4xl font-bold text-white">
-                Guruhingiz: <span className="text-white">{categoryData.name || ''}</span>
-              </h2>
-              <p className="text-white mt-2 font-semibold">Darslarni hoziroq boshlang</p>
+              <SlightFlip
+                word={`Guruhingiz: ${categoryData.name}`}
+                className="text-4xl font-bold text-black" />
+              <p className="text-black mt-2 font-semibold">Darslarni hoziroq boshlang</p>
             </div>
-          </div>
+          </ShineBorder>
 
           {accordionData.length > 0 ? (
             accordionData.map((moduleData: any) => (
               <AccordionItem
-                key={moduleData.id} 
+                key={moduleData.id}
                 id={moduleData.id}
                 title={moduleData.title}
                 icon={moduleData.icon}
-                isOpen={openAccordionId === moduleData.id} 
+                isOpen={openAccordionId === moduleData.id}
                 toggle={() => toggleAccordion(moduleData.id)}
               >
               </AccordionItem>
