7dd4e631175c0eccef8942a7d166c33b5ef87c82
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-09-14 17:28:16 +0500

    tokenda marge berdi

--- src/context/api/token.ts
+++ src/context/api/token.ts
@@ -4,16 +4,5 @@ export const config = {
     }
 }
 
-<<<<<<< HEAD
-export const setConfig = () => config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-=======
-export const imgConfig = {
-    headers: {
-        'multipart/type': 'multipart/form-data',
-        Authorization: `Bearer ${localStorage.getItem('token')}`,
-    }
-}
 
 export const setConfig = () => config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-export const setImgConfig = () => imgConfig.headers.Authorization = `Bearer ${localStorage.getItem('token')}`   
->>>>>>> c4fb316ff0b394011b49cc1bdf5a67fbd182177a
